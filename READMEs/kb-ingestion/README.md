c9c4fc627dfe03945d42757b94f6768ee4563e12
Taddy <thaddadavis@gmail.com>
2025-11-17 13:46:40 +0000

    connecting the AI School Agent with the AI School QnA KB

---
+++ READMEs/kb-ingestion/README.md
@@ -0,0 +1,7 @@
+# TLDR
+
+No, it's related to how the background process for knowledge ingestion works.
+
+Long story short, it's implemented as Google Cloud Functions.
+
+https://console.cloud.google.com/run?deploymentType=function&project=kalygo-436411
--- src/routers/aiSchoolAgent/router.py
+++ src/routers/aiSchoolAgent/router.py
@@ -4,8 +4,16 @@ from fastapi import APIRouter
 from .kb_stats import router as kb_stats_router
 from .completion import router as completion_router
 
+from .upload import router as upload_router
+from .delete_vectors import router as delete_vectors_router
+from .search import router as search_router
+
 router = APIRouter()
 
 # Include all the endpoint routers
 router.include_router(kb_stats_router)
 router.include_router(completion_router)
+
+router.include_router(upload_router)
+router.include_router(delete_vectors_router)
+router.include_router(search_router) 
