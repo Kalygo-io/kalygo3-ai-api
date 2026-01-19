44eb5cc2f35a27f2feb76a0cf163a0e44ebc1f08
Taddy <thaddadavis@gmail.com>
2026-01-19 04:12:46 +0000

    fix missing import

--- src/routers/agents/router.py
+++ src/routers/agents/router.py
@@ -11,7 +11,6 @@ from .get import router as get_router
 from .update import router as update_router
 from .delete import router as delete_router
 from .completion import router as completion_router
-from .debug import router as debug_router
 
 router = APIRouter()
 
@@ -22,4 +21,3 @@ router.include_router(get_router)
 router.include_router(update_router)
 router.include_router(delete_router)
 router.include_router(completion_router)
-router.include_router(debug_router)
