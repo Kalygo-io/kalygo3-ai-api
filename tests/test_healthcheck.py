9900d7c8a29bd1eb04055078cf5ce4277ce2a85e
Taddy <thaddadavis@gmail.com>
2026-04-28 13:58:10 +0000

    test failed test

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -7,4 +7,5 @@ from httpx import AsyncClient
 async def test_health_check_returns_200(client: AsyncClient):
     response = await client.get("/")
     assert response.status_code == 200
+    assert False
     assert response.json() == {"status": "OK!"}
